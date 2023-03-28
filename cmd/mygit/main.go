f7a5744e464113bce855cc932b61caaadef86e96
grumpydog <me@grumpydog.space>
2023-03-28 20:48:57 +0600

    feat: create a blob object

--- cmd/mygit/main.go
+++ cmd/mygit/main.go
@@ -3,15 +3,19 @@ package main
 import (
 	"bytes"
 	"compress/zlib"
+	"crypto/sha1"
+	"encoding/hex"
 	"flag"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"log"
 	"os"
 )
 
 var (
-	p = flag.String("p", "", "")
+	p = flag.String("p", "", "object which contents to print")
+	w = flag.String("w", "", "file which contents to write to the objects database")
 )
 
 // Usage: your_git.sh <command> <arg1> <arg2> ...
@@ -64,10 +68,44 @@ func main() {
 		if _, err := io.Copy(&contents, zlibReader); err != nil {
 			log.Fatal("cat-file: error")
 		}
-
 		contents.ReadBytes('\x00')
-
 		fmt.Print(contents.String())
+
+	case "hash-object":
+		if w == nil {
+			log.Fatal("hash-object: -w parameter required")
+		}
+		// read file once to both compute sha-1 sum and compress using zlib
+		// with larger files, using os.Open might be better
+		contents, err := ioutil.ReadFile(*w)
+		if err != nil {
+			log.Fatal("hash-object: unable to open file")
+		}
+		// https://alblue.bandlem.com/2011/08/git-tip-of-week-objects.html
+		prefix := fmt.Sprintf("blob %d\x00", len(contents))
+		data := append([]byte(prefix), contents...)
+
+		hasher := sha1.New()
+		hasher.Write(data)
+		objectName := hex.EncodeToString(hasher.Sum(nil))
+
+		dirName, fileName := objectName[:2], objectName[2:]
+		if err := os.MkdirAll(fmt.Sprintf(".git/objects/%s", dirName), 0755); err != nil {
+			log.Fatal("hash-object: unable to create parent object directory")
+		}
+
+		compressed := bytes.NewBuffer(nil)
+		zlibWriter := zlib.NewWriter(compressed)
+		if _, err := zlibWriter.Write(data); err != nil {
+			log.Fatal("hash-object: failed to compress the file")
+		}
+		if err := zlibWriter.Close(); err != nil {
+			log.Fatal("hash-object: failed to flush the compressor")
+		}
+		if err := os.WriteFile(fmt.Sprintf(".git/objects/%s/%s", dirName, fileName), compressed.Bytes(), 0644); err != nil {
+			log.Fatal("hash-object: failed to write to object file")
+		}
+		fmt.Println(objectName)
 
 	default:
 		fmt.Fprintf(os.Stderr, "Unknown command %s\n", command)
